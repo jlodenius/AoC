9363672fef749cea5290f13c275c9c39e70a79b2
Jacob <jacoblodenius@gmail.com>
2023-01-17 16:38:38 +0100

    day13 almost done

--- 2022/src/bin/day13/main.rs
+++ 2022/src/bin/day13/main.rs
@@ -1,17 +1,26 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
-use std::{cmp::Ordering, fmt::Debug};
+use std::{cmp::Ordering, fmt::Debug, fmt::Formatter};
 
 // Deserialize is required by serde
-#[derive(PartialEq, Deserialize)]
+#[derive(Eq, PartialEq, Clone, Deserialize)]
 #[serde(untagged)] // To represent this enum without tags, more info: https://serde.rs/enum-representations.html
 enum Node {
     Int(u32),
     List(Vec<Node>),
 }
 
+impl Node {
+    fn as_list<T>(&self, one_shot: impl FnOnce(&[Node]) -> T) -> T {
+        match self {
+            Self::Int(val) => one_shot(&[Self::Int(*val)]),
+            Self::List(val) => one_shot(&val[..]),
+        }
+    }
+}
+
 impl Debug for Node {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Int(val) => write!(f, "{val}"),
             Self::List(val) => f.debug_list().entries(val).finish(),
@@ -20,21 +29,35 @@ impl Debug for Node {
 }
 
 impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match (self, other) {
-            (Node::Int(a), Node::Int(b)) => a.partial_cmp(b),
+            (Node::Int(a), Node::Int(b)) => {
+                println!("comparing {} to {}", a, b);
+                return a.partial_cmp(b);
+            }
             (left, right) => {
-                match left {
-                    Node::Int(val) => {
-                        println!("in partial_cmp INT {:?}", val);
-                    }
-                    Node::List(val) => {
-                        println!("in partial_cmp LIST {:?}", val);
-                    }
-                }
-                return Some(Ordering::Equal);
+                // Type parameter here is not necessary, it can be infered
+                return Some(left.as_list::<Ordering>(|l| {
+                    right.as_list::<Ordering>(|r| {
+                        l.iter()
+                            // Iterate over left and right (combined with zip)
+                            .zip(r.iter())
+                            // And compare the values...
+                            .map(|(ll, rr)| ll.cmp(rr))
+                            // Find first value that is not Equal
+                            .find(|&ord| ord != Ordering::Equal)
+                            // Or if everything equal, compare the lengths of the lists
+                            .unwrap_or_else(|| l.len().cmp(&r.len()))
+                    })
+                }));
             }
         }
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
     }
 }
 
@@ -49,8 +72,8 @@ fn part_one() {
 
         println!("Left {:?} Right {:?}", left, right);
 
-        let test = left.partial_cmp(&right);
-        println!("Test -> {:?}", test);
+        let test = left > right;
+        println!("left > right = {:?}", test);
     }
 }
 
