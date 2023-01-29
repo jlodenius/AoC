9f98db2bcc6a941c4879efd026aa75f1745cf999
Jacob <jacoblodenius@gmail.com>
2023-01-29 01:00:55 +0100

    complete day15

--- 2022/README.md
+++ 2022/README.md
@@ -9,3 +9,8 @@
 > Using serde json to parse input into a custom Enum `Node`.
 > Implementing the `Ord` and `PartialOrd` traits to sort a vector of `Node`
 > Implementing a custom `Debug` fn to print `Node`
+
+### Day 15
+> Parsing strings with nom. Struggled a bit with performance in part 2.
+> Another one to possibly come back to in the future. Not so much Rust
+> issues though.
--- 2022/src/bin/day15/main.rs
+++ 2022/src/bin/day15/main.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
+
 use nom::{
     bytes::streaming::tag,
     combinator::map,
@@ -5,26 +8,34 @@ use nom::{
     IResult,
 };
 
-struct Line {
+#[derive(Debug)]
+struct InputData {
     sensor: Point,
     beacon: Point,
+    manhattan_d: i64,
 }
 
-impl Line {
-    fn parse(i: &str) -> IResult<&str, Line> {
-        // Maps a fn on the result of a parser
+impl InputData {
+    fn parse(i: &str) -> IResult<&str, InputData> {
         map(
-            // The parser
             separated_pair(
                 preceded(tag("Sensor at "), Point::parse),
                 tag(": closest beacon is at "),
                 Point::parse,
             ),
-            |(sensor, beacon)| Line { sensor, beacon },
+            |(sensor, beacon)| {
+                let manhattan_d = sensor.manhattan_d(&beacon);
+                InputData {
+                    sensor,
+                    beacon,
+                    manhattan_d,
+                }
+            },
         )(i)
     }
 }
 
+#[derive(Debug)]
 struct Point {
     x: i64,
     y: i64,
@@ -32,20 +43,158 @@ struct Point {
 
 impl Point {
     fn parse(i: &str) -> IResult<&str, Point> {
+        // Maps a function on the result of a parser
         map(
+            // Combines the result of two parser between a separator
+            // and returns a tuple of the two results
             separated_pair(
+                // Discards the first parsers result and returns the second
                 preceded(tag("x="), nom::character::complete::i64),
                 tag(", "),
                 preceded(tag("y="), nom::character::complete::i64),
             ),
+            // The map function
             |(x, y)| Point { x, y },
         )(i)
     }
+
+    fn manhattan_d(&self, other: &Point) -> i64 {
+        return (self.x.abs_diff(other.x) + self.y.abs_diff(other.y)) as i64;
+    }
 }
 
-// Sensor at x=1259754, y=1927417: closest beacon is at x=1174860, y=2000000
-fn main() {
+fn part_one() {
+    // Parse input
+    let mut input_data: Vec<InputData> = vec![];
     for line in include_str!("input.txt").lines() {
-        println!("{}", line);
+        let line = InputData::parse(line).unwrap().1;
+        input_data.push(line);
     }
+
+    // Do calculations
+    let mut unavailable: HashSet<i64> = HashSet::new();
+    let y_to_search: i64 = 2_000_000;
+
+    for input in input_data.iter() {
+        // Search outwards from position (sensor.x, y_to_find)
+        for x in input.sensor.x..=(input.sensor.x + input.manhattan_d) {
+            let p = Point { x, y: y_to_search };
+            if input.sensor.manhattan_d(&p) > input.manhattan_d {
+                break;
+            };
+
+            unavailable.insert(x);
+            unavailable.insert(input.sensor.x - x.abs_diff(input.sensor.x) as i64);
+        }
+    }
+    // Remove all known beacons on the y_to_find-line
+    // ...apparently only necessary for the test input but lets keep it anyway
+    let beacons_at_y: Vec<_> = input_data
+        .iter()
+        .filter(|i| i.beacon.y == y_to_search)
+        .map(|i| &i.beacon)
+        .collect();
+    for beacon in beacons_at_y.iter() {
+        unavailable.remove(&beacon.x);
+    }
+
+    println!("Answer: {:?}", unavailable.len());
+}
+
+fn part_two() {
+    // Parse input
+    let mut input_data: Vec<InputData> = vec![];
+    for line in include_str!("input.txt").lines() {
+        let line = InputData::parse(line).unwrap().1;
+        input_data.push(line);
+    }
+
+    let max: i64 = 4_000_000;
+    // let max: i64 = 20;
+
+    // Key = Y
+    // Value = impossible x range (min, max)
+    let mut impossible_x_ranges: HashMap<i64, Vec<(i64, i64)>> = HashMap::new();
+
+    // Go through each Y
+    for y in 0..=max {
+        if y % 1_000_000 == 0 {
+            println!("First loop {y}");
+        }
+        // Determine which X coordinates are impossible
+        // and adds them to map of impossible x values
+        for input in input_data.iter() {
+            // No reason to continue
+            if input.manhattan_d < input.sensor.y.abs_diff(y) as i64 {
+                continue;
+            }
+
+            // Create ranges of impossible X values for this Y
+            // Have to check recursively with large jumps here to improve performance
+            // Checking each X one by one like in part_one would take forever
+            let from = input.sensor.x;
+            let (x_min, x_max) = get_x_min_max(from, y, &input);
+            let map_entry = impossible_x_ranges.entry(y).or_insert(vec![]);
+            map_entry.push((x_min, x_max));
+        }
+    }
+
+    // Go through each Y and check against each range of X
+    for y in 0..=max {
+        if y % 1_000_000 == 0 {
+            println!("Second loop {y}");
+        }
+        let ranges = impossible_x_ranges.get_mut(&y).unwrap();
+        match find_unconnected_range(ranges) {
+            Some(x) => {
+                // We will only get one match, and that will be our X coordinate
+                println!("X is {x}");
+                let tuning_frequency = (x * 4_000_000) + y;
+                println!("Tuning frequency is {}", tuning_frequency);
+                break;
+            }
+            None => {} // This means the entire range is "connected"
+        }
+    }
+}
+
+fn get_x_min_max(x: i64, y: i64, input: &InputData) -> (i64, i64) {
+    let p = Point { x, y };
+    let current_manhattan_d = input.sensor.manhattan_d(&p);
+    let delta = current_manhattan_d.abs_diff(input.manhattan_d) as i64;
+    match current_manhattan_d.cmp(&input.manhattan_d) {
+        Ordering::Equal => {
+            let x_max = x;
+            let x_min = input.sensor.x - x_max.abs_diff(input.sensor.x) as i64;
+            return (x_min, x_max);
+        }
+        Ordering::Less => {
+            return get_x_min_max(x + delta, y, input);
+        }
+        Ordering::Greater => {
+            return get_x_min_max(x - delta, y, input);
+        }
+    }
+}
+
+fn find_unconnected_range(ranges: &mut Vec<(i64, i64)>) -> Option<i64> {
+    ranges.sort_by_key(|r| r.0);
+
+    let (start, rest) = ranges.split_first().unwrap();
+    let mut max: i64 = start.1;
+
+    for range in rest.iter() {
+        if range.0 > max + 1 {
+            return Some(max + 1);
+        }
+        max = std::cmp::max(max, range.1);
+    }
+    return None;
+}
+
+fn main() {
+    println!("--- PART ONE ---");
+    part_one();
+    println!("--- PART TWO ---");
+    part_two();
 }
