d9886337f1e0ff92c660bdd1af5e5496ce69ec6f
Jacob <jacoblodenius@gmail.com>
2023-02-28 22:15:58 +0100

    refactor

--- 2022/src/bin/day18/main.rs
+++ 2022/src/bin/day18/main.rs
@@ -35,118 +35,26 @@ impl PartialEq for Coordinate {
 
 impl Coordinate {
     fn exposed_sides(&self, cm: &HashMap<[i32; 3], Coordinate>) -> i32 {
-        let mut unexposed = 0;
-
-        // check x
-        if let Some(_) = cm.get(&[self.x - 1, self.y, self.z]) {
-            unexposed += 1
-        }
-        if let Some(_) = cm.get(&[self.x + 1, self.y, self.z]) {
-            unexposed += 1
-        }
-
-        // check y
-        if let Some(_) = cm.get(&[self.x, self.y - 1, self.z]) {
-            unexposed += 1
-        }
-        if let Some(_) = cm.get(&[self.x, self.y - 1, self.z]) {
-            unexposed += 1
-        }
-
-        // check z
-        if let Some(_) = cm.get(&[self.x, self.y, self.z - 1]) {
-            unexposed += 1
-        }
-        if let Some(_) = cm.get(&[self.x, self.y, self.z + 1]) {
-            unexposed += 1
-        }
-
+        let unexposed: i32 = [-1, 1]
+            .iter()
+            .map(|add_to| {
+                let mut val = 0;
+                // check x
+                if let Some(_) = cm.get(&[self.x + add_to, self.y, self.z]) {
+                    val += 1
+                }
+                // check y
+                if let Some(_) = cm.get(&[self.x, self.y + add_to, self.z]) {
+                    val += 1
+                }
+                // check z
+                if let Some(_) = cm.get(&[self.x, self.y, self.z + add_to]) {
+                    val += 1
+                }
+                val
+            })
+            .sum();
         return 6 - unexposed;
-    }
-
-    fn exposed_sides_2(&self, cm: &HashMap<[i32; 3], Coordinate>) -> i32 {
-        let search_width = 50;
-        let mut exposed = 0;
-
-        // check x
-        if let None = cm.get(&[self.x - 1, self.y, self.z]) {
-            // Check further
-            let start = self.x - search_width;
-            let end = self.x - 1;
-            let is_exposed = (start..end)
-                .map(|i| cm.get(&[i, self.y, self.z]))
-                .all(|val| val.is_none());
-
-            if is_exposed {
-                exposed += 1
-            }
-        }
-        if let None = cm.get(&[self.x + 1, self.y, self.z]) {
-            // Check further
-            let start = self.x + 1;
-            let end = self.x + search_width;
-            let is_exposed = (start..end)
-                .map(|i| cm.get(&[i, self.y, self.z]))
-                .all(|val| val.is_none());
-
-            if is_exposed {
-                exposed += 1
-            }
-        }
-
-        // check y
-        if let None = cm.get(&[self.x, self.y - 1, self.z]) {
-            // Check further
-            let start = self.y - search_width;
-            let end = self.y - 1;
-            let is_exposed = (start..end)
-                .map(|i| cm.get(&[self.x, i, self.z]))
-                .all(|val| val.is_none());
-
-            if is_exposed {
-                exposed += 1
-            }
-        }
-        if let None = cm.get(&[self.x, self.y - 1, self.z]) {
-            // Check further
-            let start = self.y + 1;
-            let end = self.y + search_width;
-            let is_exposed = (start..end)
-                .map(|i| cm.get(&[self.x, i, self.z]))
-                .all(|val| val.is_none());
-
-            if is_exposed {
-                exposed += 1
-            }
-        }
-
-        // check z
-        if let None = cm.get(&[self.x, self.y, self.z - 1]) {
-            // Check further
-            let start = self.z - search_width;
-            let end = self.z - 1;
-            let is_exposed = (start..end)
-                .map(|i| cm.get(&[self.x, self.y, i]))
-                .all(|val| val.is_none());
-
-            if is_exposed {
-                exposed += 1
-            }
-        }
-        if let None = cm.get(&[self.x, self.y, self.z + 1]) {
-            // Check further
-            let start = self.z + 1;
-            let end = self.z + search_width;
-            let is_exposed = (start..end)
-                .map(|i| cm.get(&[self.x, self.y, i]))
-                .all(|val| val.is_none());
-
-            if is_exposed {
-                exposed += 1
-            }
-        }
-
-        return exposed;
     }
 }
 
@@ -174,12 +82,7 @@ fn part_two() {
         coordinates_map.insert([coordinate.x, coordinate.y, coordinate.z], coordinate);
     }
 
-    let exposed_sides: i32 = coordinates_map
-        .iter()
-        .map(|(_, coordinate)| coordinate.exposed_sides_2(&coordinates_map))
-        .sum();
-
-    println!("exposed_sides {exposed_sides}");
+    // TODO
 }
 
 fn main() {
